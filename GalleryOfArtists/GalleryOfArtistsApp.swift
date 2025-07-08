e31b91b76aecabda0f413875f07944d62b59cbf8
SnakCat <snakkdss@gmail.com>
2025-07-08 20:02:23 +0300

    initial commit

---
+++ GalleryOfArtists/GalleryOfArtistsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryOfArtistsApp.swift
+//  GalleryOfArtists
+//
+//  Created by DimaTru on 08.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct GalleryOfArtistsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

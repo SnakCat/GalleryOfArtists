8f2ea5971806a311c6e0b7cf85c56be29177bf50
SnakCat <snakkdss@gmail.com>
2025-07-08 20:16:17 +0300

    create network layer

---
+++ GalleryOfArtists/NetworkService.swift
@@ -0,0 +1,29 @@
+//
+//  NetworkService.swift
+//  GalleryOfArtists
+//
+//  Created by DimaTru on 08.07.2025.
+//
+
+import Foundation
+
+protocol NetworkServiceProtocol {
+    func fetchData() async throws -> [ArtistModel]
+}
+
+final class NetworkService: NetworkServiceProtocol {
+    
+    private let baseURL = "https://cdn.accelonline.io/OUR6G_IgJkCvBg5qurB2Ag/files/YPHn3cnKEk2NutI6fHK04Q.json"
+    
+    func fetchData() async throws -> [ArtistModel] {
+        guard let url = URL(string: baseURL) else {
+            throw URLError(.badURL)
+        }
+        let (data, responce) = try await URLSession.shared.data(from: url)
+        if let httpResponce = responce as? HTTPURLResponse, httpResponce.statusCode != 200 {
+            fatalError("status code \(httpResponce.statusCode)")
+        }
+        let artists = try JSONDecoder().decode([ArtistModel].self, from: data)
+        return artists
+    }
+}

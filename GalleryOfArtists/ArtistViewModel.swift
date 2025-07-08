7572cde03add63e489cd3c161119d1ea8124758b
SnakCat <snakkdss@gmail.com>
2025-07-08 20:23:35 +0300

    created view model

---
+++ GalleryOfArtists/ArtistViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  ArtistViewModel.swift
+//  GalleryOfArtists
+//
+//  Created by DimaTru on 08.07.2025.
+//
+
+import Foundation
+
+@MainActor
+final class ArtistViewModel: ObservableObject {
+    
+    @Published var artists: [ArtistModel] = []
+    @Published var errorMessage: String?
+    @Published var isLoading = false
+    
+    private let networkService = NetworkService()
+    
+    func fetchAtrists() async {
+        isLoading = true
+        do {
+            let fetchArtists = try await networkService.fetchData()
+            self.artists = fetchArtists
+        } catch {
+            self.errorMessage = "Error loading \(error.localizedDescription)"
+        }
+        isLoading = false
+    }
+}
